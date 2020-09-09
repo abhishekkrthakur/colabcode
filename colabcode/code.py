a4907fca555c0d23312d5b9c99db4fe23670207d
Skander HADJ ROMDHAN <37614760+Skander-hadjromdhan@users.noreply.github.com>
2020-09-09 16:02:48 +0200

    handle not colab env

--- colabcode/code.py
+++ colabcode/code.py
@@ -1,24 +1,29 @@
 import os
 import subprocess
 from pyngrok import ngrok
-from google.colab import drive
+try:
+    from google.colab import drive
+    colab_env = True
+catch ModuleNotFoundError:
+    print("Not in google colab envirement, google drive will can not be mounted")
+    colab_env = False
 
 
 class ColabCode:
     def __init__(self, port=10000, password=None, gcloud=False):
         self.port = port
         self.password = password
-        self.gcloud = gcloud
+        self._mount = gcloud
         self._install_code()
         self._start_server()
         self._run_code()
-        
+
     def _install_code(self):
         subprocess.run(
             ["wget", "https://code-server.dev/install.sh"], stdout=subprocess.PIPE
         )
         subprocess.run(["sh", "install.sh"], stdout=subprocess.PIPE)
-        
+
     def _start_server(self):
         active_tunnels = ngrok.get_tunnels()
         for tunnel in active_tunnels:
@@ -26,10 +31,10 @@ class ColabCode:
             ngrok.disconnect(public_url)
         url = ngrok.connect(port=self.port)
         print(f"Code Server can be accessed on: {url}")
-        
+
     def _run_code(self):
         os.system(f"fuser -n tcp -k {self.port}")
-        if self.gcloud:
+        if self._mount and colab_env:
             drive.mount("/content/drive")
         if self.password:
             code_cmd = f"PASSWORD={self.password} code-server --port {self.port} --disable-telemetry"
