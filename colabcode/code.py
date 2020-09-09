6786c5ba13d19d8571b6e30765c0fac225ea05ba
Reshinth Adithyan <36307201+reshinthadithyan@users.noreply.github.com>
2020-09-09 21:32:46 +0530

    Update code.py
    cloning github repo to the root

--- colabcode/code.py
+++ colabcode/code.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 from pyngrok import ngrok
+import git
 try:
     from google.colab import drive
     colab_env = True
@@ -9,10 +10,11 @@ except ImportError:
 
 
 class ColabCode:
-    def __init__(self, port=10000, password=None, gcloud=False):
+    def __init__(self, port=10000, password=None, gcloud=False,github_repo=False):
         self.port = port
         self.password = password
         self._mount = gcloud
+        self.github_repo = github_repo 
         self._install_code()
         self._start_server()
         self._run_code()
@@ -22,7 +24,9 @@ class ColabCode:
             ["wget", "https://code-server.dev/install.sh"], stdout=subprocess.PIPE
         )
         subprocess.run(["sh", "install.sh"], stdout=subprocess.PIPE)
-
+    def clone_repo(self):
+        '''Clone a Github Repo to the root directory'''
+        git.Git("/").clone(self.github_repo)
     def _start_server(self):
         active_tunnels = ngrok.get_tunnels()
         for tunnel in active_tunnels:
@@ -35,10 +39,13 @@ class ColabCode:
         os.system(f"fuser -n tcp -k {self.port}")
         if self._mount and colab_env:
             drive.mount("/content/drive")
+        if self.github_repo:
+            self.clone_repo()
         if self.password:
             code_cmd = f"PASSWORD={self.password} code-server --port {self.port} --disable-telemetry"
         else:
             code_cmd = f"code-server --port {self.port} --auth none --disable-telemetry"
+        
         with subprocess.Popen(
             [code_cmd],
             shell=True,
