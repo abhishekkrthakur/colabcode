f87bb90174be978a0dd0a6436558627b2ce37515
Skander-hadjromdhan <37614760+Skander-hadjromdhan@users.noreply.github.com>
2020-09-09 15:01:21 +0200

    Add mounting google drive
    Additional feature: mount google drive to have permanent storage

--- colabcode/code.py
+++ colabcode/code.py
@@ -1,22 +1,22 @@
 import os
 import subprocess
 from pyngrok import ngrok
+from google.colab import drive
 
 
 class ColabCode:
-    def __init__(self, port=10000, password=None):
+    def __init__(self, port=10000, password=None, gcloud=False):
         self.port = port
         self.password = password
+        self.gcloud = gcloud
         self._install_code()
         self._start_server()
         self._run_code()
-
     def _install_code(self):
         subprocess.run(
             ["wget", "https://code-server.dev/install.sh"], stdout=subprocess.PIPE
         )
         subprocess.run(["sh", "install.sh"], stdout=subprocess.PIPE)
-
     def _start_server(self):
         active_tunnels = ngrok.get_tunnels()
         for tunnel in active_tunnels:
@@ -24,9 +24,10 @@ class ColabCode:
             ngrok.disconnect(public_url)
         url = ngrok.connect(port=self.port)
         print(f"Code Server can be accessed on: {url}")
-
     def _run_code(self):
         os.system(f"fuser -n tcp -k {self.port}")
+        if self.gcloud:
+            drive.mount("/content/drive")
         if self.password:
             code_cmd = f"PASSWORD={self.password} code-server --port {self.port} --disable-telemetry"
         else:
