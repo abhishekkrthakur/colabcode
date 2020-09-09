6f71da7f3fb349c34fa2f7d69750ab1c6561d9c5
abhishekkrthakur <abhishek4@gmail.com>
2020-09-09 19:02:55 +0200

    bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", encoding="utf-8") as f:
 if __name__ == "__main__":
     setup(
         name="colabcode",
-        version="0.0.4",
+        version="0.0.5",
         description="ColabCode - Run codeserver on Colab!",
         long_description=long_description,
         long_description_content_type="text/markdown",
