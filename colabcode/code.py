0147ddc16d21ed4697695fc808d3d780cc574ecb
someman Budhathoki <44607279+xettrisomeman@users.noreply.github.com>
2021-01-06 19:09:36 +0545

    User can add absolute path

--- colabcode/code.py
+++ colabcode/code.py
@@ -15,14 +15,15 @@ CODESERVER_VERSION = "3.7.4"
 
 
 class ColabCode:
-    def __init__(self, port=10000, password=None, authtoken=None, mount_drive=False):
+    def __init__(self, port=10000, password=None, authtoken=None, mount_drive=False, drive_path=None):
         self.port = port
         self.password = password
         self.authtoken = authtoken
         self._mount = mount_drive
+        self._default_path = "/content/drive"       
         self._install_code()
         self._install_extensions()
-        self._start_server()
+        self._start_server(drive_path)
         self._run_code()
 
     def _install_code(self):
@@ -34,11 +35,14 @@ class ColabCode:
             stdout=subprocess.PIPE,
         )
 
+
     def _install_extensions(self):
         for ext in EXTENSIONS:
             subprocess.run(["code-server", "--install-extension", f"{ext}"])
 
-    def _start_server(self):
+
+
+    def _start_server(self, drive_path):
         if self.authtoken:
             ngrok.set_auth_token(self.authtoken)
         active_tunnels = ngrok.get_tunnels()
@@ -46,12 +50,26 @@ class ColabCode:
             public_url = tunnel.public_url
             ngrok.disconnect(public_url)
         url = ngrok.connect(addr=self.port, options={"bind_tls": True})
-        print(f"Code Server can be accessed on: {url}")
+        print(self._mount_drive(url, drive_path))
+
+
+    def _mount_drive(self, url, drive_path):
+        if self._mount and colab_env:
+            if drive_path:
+                try :
+                    drive.mount(drive_path)
+                    self._default_path = drive_path
+                except:
+                    drive.mount(self._default_path)
+            else:
+                drive.mount(self._default_path)
+            return f"Code server can be accessed on : {url}/?folder={self._default_path}"
+        return f"Code server can be accessed on : {url}"
+
+        
 
     def _run_code(self):
         os.system(f"fuser -n tcp -k {self.port}")
-        if self._mount and colab_env:
-            drive.mount("/content/drive")
         if self.password:
             code_cmd = f"PASSWORD={self.password} code-server --port {self.port} --disable-telemetry"
         else:
